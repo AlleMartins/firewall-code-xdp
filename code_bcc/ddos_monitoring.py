b317c576bdfa8621c747a241e3bcc7120e66dbb6
Alessandro <alessandro.martini10@studio.unibo.it>
2024-09-11 15:45:16 +0200

    prima versione firewall, blocca semplicemente gli ip che fanno troppe richieste

--- code_bcc/ddos_monitoring.py
+++ code_bcc/ddos_monitoring.py
@@ -11,7 +11,7 @@ bpf_text = """
 #include <linux/ip.h>
 #include <linux/tcp.h>
 
-#define MAX_REQUESTS 100000
+#define MAX_REQUESTS 15
 
 __attribute__((section("xdp"), used))
 int xdp_firewall(struct xdp_md *ctx);
@@ -29,7 +29,7 @@ int xdp_firewall(struct xdp_md *ctx) {
         return XDP_ABORTED;
     }
 
-    if (ether->h_proto != 0x08U) {  // htons(ETH_P_IP) -> 0x08U
+    if (ether->h_proto != htons(ETH_P_IP)) {
         // Non IPv4 traffic
         return XDP_PASS;
     }
@@ -68,9 +68,14 @@ int xdp_firewall(struct xdp_md *ctx) {
 
 b = BPF(text=bpf_text)
 
-# Carica il programma XDP sull'interfaccia di rete (es. "eth0") --> wlp5s0
+# Carica il programma XDP sull'interfaccia di rete (es. "eth0")
 device = "wlp5s0"
 b.attach_xdp(device, b.load_func("xdp_firewall", BPF.XDP))
+
+# Funzione per convertire l'IP da intero a formato CIDR
+def int_to_ip(ipnum):
+    ipnum = socket.ntohl(ipnum)  # Inverte l'endianness
+    return socket.inet_ntoa(struct.pack("!I", ipnum))
 
 # Stampa intestazione
 print("Monitoring for possible DDOS attacks...")
@@ -78,7 +83,11 @@ print("Monitoring for possible DDOS attacks...")
 # Loop infinito per continuare a leggere gli eventi
 while True:
     try:
-        b.trace_print()
+        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
+        msg = msg.decode('utf-8')  # Decodifica il messaggio da bytes a stringa
+        if "Blocked IP" in msg:
+            ip_num = int(msg.split("Blocked IP: ")[1], 16)
+            print(f"Blocked IP: {int_to_ip(ip_num)}")
     except KeyboardInterrupt:
         print("Interrupted, exiting...")
         break
